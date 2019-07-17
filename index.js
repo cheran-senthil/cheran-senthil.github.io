de40a8dd887b775077ed116c4fbc2b4b22dd78a4
Cheran <cheran.v.senthil@gmail.com>
2019-07-17 16:50:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const gravatarEmail = 'cheran.v.senthil@gmail.com'
+
+const userIds = {
+  codeforces: 'c1729',
+  email: 'cheran.v.senthil@gmail.com',
+  github: 'cheran-senthil',
+  linkedin: 'cheran-senthil',
+}
+
+const link = {
+  codeforces: `https://codeforces.com/profile/${userIds.codeforces}`,
+  email: `mailto:${userIds.email}`,
+  github: `https://github.com/${userIds.github}`,
+  linkedin: `https://www.linkedin.com/in/${userIds.linkedin}`,
+  resume: `./resume.pdf`,
+}
+
+const name = {
+  codeforces: 'Codeforces',
+  email: 'Email',
+  github: 'GitHub',
+  linkedin: 'LinkedIn',
+  resume: 'Resume',
+}
+
+const linksOrder = ['email', 'github', 'linkedin', 'codeforces', 'resume']
+
+const gravatarHash = CryptoJS.MD5(gravatarEmail.toLowerCase())
+const profileImage = document.createElement('img')
+profileImage.src = `https://gravatar.com/avatar/${gravatarHash}?s=200`
+profileImage.className = 'profile-image'
+
+let links = document.createElement('div')
+links.className = 'links'
+
+linksOrder.forEach(type => {
+  const linkElement = document.createElement('a')
+  linkElement.className = 'link'
+  linkElement.href = link[type]
+
+  const image = document.createElement('img')
+  image.src = `./images/${type}.png`
+
+  linkElement.appendChild(image)
+  linkElement.appendChild(document.createTextNode('  ' + name[type]))
+
+  links.appendChild(linkElement)
+})
+
+document.body.appendChild(profileImage)
+document.body.appendChild(document.createElement('br'))
+document.body.appendChild(links)
